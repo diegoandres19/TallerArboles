a2ead980929d915ff950ce1effd91551b6e6fb60
Carlos Camacho <beresikcamacho@gmail.com>
2023-11-14 23:31:31 -0500

    Árboles
    Corrección del código, cambios importantes como añadir un menú, código más legible y de acuerdo a los temas revisados en clase.

--- main.py
+++ main.py
@@ -1,110 +1,143 @@
-import matplotlib.pyplot as plt
+class Nodo:
+    def __init__(self, value):
+        self.dato = value
+        self.izquierda = None
+        self.derecha = None
 
-class NodoArbol:
-    def __init__(self, value, left=None, right=None):
-        self.data = value
-        self.left = left
-        self.right = right
-        
-class BinarySearchTree:
-    def __init__(self):
-        self.__root = None
-        
-    def insert(self, value):
-        
-        if self.__root == None:
-            self.__root = NodoArbol(value)
-        
-        else:
-            self.__insert__(self.__root, value)
-            
-    def __insert__(self, nodo, value):
-        if nodo.data == value:
-            print("! dato existente, no se ingresa nada")
-        elif value < nodo.data:
+class Arbol:
+    def __init__(self, dato):
+        self.raiz = Nodo(dato)
 
-            if nodo.left == None:
-                nodo.left = NodoArbol(value)
-
+    def agregarRecursivo(self, nodo, dato):
+        if dato < nodo.dato:
+            if nodo.izquierda is None:
+                nodo.izquierda = Nodo(dato)
             else:
-                self.__insert__(nodo.left, value)
+                self.agregarRecursivo(nodo.izquierda, dato)
         else:
-            if nodo.right == None:
-                nodo.right = NodoArbol(value)
+            if nodo.derecha is None:
+                nodo.derecha = Nodo(dato)
             else:
-                self.__insert__(nodo.right, value)
-                
+                self.agregarRecursivo(nodo.derecha, dato)
 
-    def peso(self, nodo):
-        if nodo == None:
+    def inorden(self, nodo):
+        if nodo is not None:
+            self.inorden(nodo.izquierda)
+            print(nodo.dato, end=", ")
+            self.inorden(nodo.derecha)
+
+    def preorden(self, nodo):
+        if nodo is not None:
+            print(nodo.dato, end=", ")
+            self.preorden(nodo.izquierda)
+            self.preorden(nodo.derecha)
+
+    def postorden(self, nodo):
+        if nodo is not None:
+            self.postorden(nodo.izquierda)
+            self.postorden(nodo.derecha)
+            print(nodo.dato, end=", ")
+
+    def buscar(self, nodo, busqueda):
+        if nodo is None:
+            return None
+        if nodo.dato == busqueda:
+            return nodo
+        if busqueda < nodo.dato:
+            return self.buscar(nodo.izquierda, busqueda)
+        else:
+            return self.buscar(nodo.derecha, busqueda)
+
+    def agregar(self, dato):
+        self.agregarRecursivo(self.raiz, dato)
+
+    def imprimirInorden(self):
+        print("Imprimiendo árbol inorden: ")
+        self.inorden(self.raiz)
+        print("")
+
+    def imprimirPreorden(self):
+        print("Imprimiendo árbol preorden: ")
+        self.preorden(self.raiz)
+        print("")
+
+    def imprimirPostorden(self):
+        print("Imprimiendo árbol postorden: ")
+        self.postorden(self.raiz)
+        print("")
+
+    def buscarDato(self, busqueda):
+        return self.buscar(self.raiz, busqueda)
+
+    def calcularAltura(self, nodo):
+        if nodo is None:
             return 0
         else:
-            return self.peso(nodo.left) + self.peso(nodo.right) + 1
-        
+            altura_izquierda = self.calcularAltura(nodo.izquierda)
+            altura_derecha = self.calcularAltura(nodo.derecha)
+            return max(altura_izquierda, altura_derecha) + 1
 
-    def orden(self, nodo):
-        if nodo == None:
+    def obtenerAltura(self):
+        return self.calcularAltura(self.raiz)
+
+    def calcularPeso(self, nodo):
+        if nodo is None:
             return 0
         else:
-            return max(self.orden(nodo.left), self.orden(nodo.right))+1
-            
+            peso_izquierda = self.calcularPeso(nodo.izquierda)
+            peso_derecha = self.calcularPeso(nodo.derecha)
+            return peso_izquierda + peso_derecha + 1
 
-    def altura(self, nodo):
-        if nodo == None:
-            return 0
+    def obtenerPeso(self):
+        return self.calcularPeso(self.raiz)
+
+arbol = Arbol(10)
+def menu():
+    print("1. Agregar elemento al árbol")
+    print("2. Imprimir árbol inorden")
+    print("3. Imprimir árbol preorden")
+    print("4. Imprimir árbol postorden")
+    print("5. Buscar elemento en el árbol")
+    print("6. Calcular altura del árbol")
+    print("7. Calcular peso del árbol")
+    print("8. Salir")
+
+    opcion = input("Selecciona una opción: ")
+
+    if opcion == "1":
+        dato = int(input("Ingrese el elemento a agregar al árbol: "))
+        arbol.agregar(dato)
+        menu()
+    elif opcion == "2":
+        arbol.imprimirInorden()
+        menu()
+
+    elif opcion == "3":
+        arbol.imprimirPreorden()
+        menu()
+    elif opcion == "4":
+        arbol.imprimirPostorden()
+        menu()
+    elif opcion == "5":
+        busqueda = int(input("Ingrese el elemento a buscar en el árbol: "))
+        resultado = arbol.buscarDato(busqueda)
+        if resultado:
+            print(f"El elemento {busqueda} está en el árbol.")
+            menu()
         else:
-            return max(self.altura(nodo.left), self.altura(nodo.right))+1
-        
-    
-
-tree = BinarySearchTree()
-
-
-while True:
-    valor = input("Ingrese el valor para el nodo (o 's' para salir): ")
-    if valor == 's':
-        break
-    tree.insert(int(valor))
-
-def plot_tree(node, ax, x, y, dx, dy):
-    if node is None:
-        return
-    
-    ax.text(x, y, str(node.data), ha='center', va='center', 
-            bbox=dict(facecolor='white', edgecolor='black', boxstyle='circle'))
-    
-    left_x = x - dx/2
-    right_x = x + dx/2
-    child_y = y - dy
-    
-    if node.left is not None:
-        ax.plot([x, left_x], [y, child_y], 'k-')
-        plot_tree(node.left, ax, left_x, child_y, dx/2, dy)
-    
-    if node.right is not None:
-        ax.plot([x, right_x], [y, child_y], 'k-')
-        plot_tree(node.right, ax, right_x, child_y, dx/2, dy)
-        
-
-peso = tree.peso(tree._BinarySearchTree__root)
-print("Peso del arbol:", peso)
-
-
-orden = tree.orden(tree._BinarySearchTree__root)
-print("Orden del arbol:", orden)
-
-
-altura = tree.altura(tree._BinarySearchTree__root)
-print("Altura del arbol:", altura)
-
-
-fig, ax = plt.subplots()
-
-
-plt.margins(0.1, 0.1)
-
-
-plot_tree(tree._BinarySearchTree__root, ax, 0, 0, 1, 1)
-
-
-plt.show()
+            print(f"El elemento {busqueda} no está en el árbol.")
+            menu()
+    elif opcion == "6":
+        altura = arbol.obtenerAltura()
+        print(f"La altura del árbol es: {altura}")
+        menu()
+    elif opcion == "7":
+        peso = arbol.obtenerPeso()
+        print(f"El peso del árbol es: {peso}")
+        menu()
+    elif opcion == "8":
+        print("Saliendo del programa... ¡Hasta la próxima!")
+    else:
+        print("Opción no válida. Por favor, selecciona una opción válida.")
+        menu()
+menu()
